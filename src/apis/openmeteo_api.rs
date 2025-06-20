a7c9470e27c6ce22b2e39850b6469204677620c2
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-06-20 07:16:45 +0000

    fix: break api endpoint into chunks

--- src/apis/openmeteo_api.rs
+++ src/apis/openmeteo_api.rs
@@ -5,7 +5,13 @@ pub async fn fetch_weather_async(lat: f32, lon: f32) -> OpenMeteoWeather {
     info!("Fetching weather data for coordinates: ({}, {})", lat, lon);
 
     let api_endpoint = format!(
-        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=Asia%2FBangkok&forecast_days=1",
+        // line is too long, so we use concat! to split it
+        concat!(
+            "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}",
+            "&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,",
+            "precipitation,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m",
+            "&timezone=Asia%2FBangkok&forecast_days=1"
+        ),
         lat, lon
     );
 
